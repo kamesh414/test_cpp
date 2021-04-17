3c66af5edfeccda87d716575cf99e311fff06d47
kamesh <kamesh414@gmail.com>
2021-04-17 22:56:12 +0530

    added comment

--- file1.cpp
+++ file1.cpp
@@ -3,6 +3,6 @@ using namespace std;
 int main()
 {
     cout<<"I am in file1.cpp file "<<endl;
-    cout<<"file1.cpp file stored in github" <<endl;
+    cout<<"file1.cpp file stored in github" <<endl; // cout syntax
     return 0;
 }
